23b63f4419126d82db2559bd5adb767be97db7af
hannguyen.31241020390-ship-it <hannguyen.31241020390@st.ueh.edu.vn>
2026-09-14 07:44:25 +0700

    Update

--- canbanghistogram.py
+++ canbanghistogram.py
@@ -3,9 +3,31 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 image= cv2.imread("C:/Users/Windows/Downloads/Fig0310(b)(washed_out_pollen_image).png", cv2.IMREAD_GRAYSCALE)
-plt.figure(figsize=(10, 4))
-plt.hist(image.ravel(), 256, [0, 256])
-plt.xlabel("Pixel Value")
-plt.ylabel("Frequency")
-plt.title("Biểu đồ Histogram của ảnh xám")
-plt.show()
+def HistogramCalculate(img):
+    W, H = img.shape
+    hr = np.zeros(256)
+    for x in range(W):
+        for y in range(H):
+            val = img[x, y]
+            hr[val] += 1
+    return hr
+def HistogramEqualization(image):
+    W, H= image.shape
+    hr= HistogramCalculate(image)
+    pr = np.zeros(256)
+    for i in range(256):
+        pr[i] = hr[i]/(W*H)
+    hr_eq = np.zeros(256)
+    for i in range(256):
+        for j in range(i+1):
+            hr_eq[i] += pr[j]
+    img_eq = np.zeros((W, H), dtype=np.uint8)
+    for i in range(W):
+        for j in range(H):
+            r= image[i][j]
+            img_eq[i][j] = hr_eq[r]*255
+    return img_eq
+cv2.imshow("Original Image", image)
+cv2.imshow("Histogram Equalization", HistogramEqualization(image))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
